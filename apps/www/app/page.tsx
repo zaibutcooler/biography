d93213aa6d0a447077e11116730adc69f7e9b077
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-01-04 16:11:41 +0630

    setting up metadata

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,3 +1,16 @@
+import profile from "@repo/data/profile";
+
 export default function Page() {
-  return <main></main>;
+  return (
+    <main
+      className={
+        "max-w-3xl mx-auto bg-stone-900 text-white flex flex-col gap-6 text-center"
+      }
+    >
+      <div>
+        <h1 className="text-5xl font-bold">Hello, World!</h1>
+        <p className="text-xl">{profile.about}</p>
+      </div>
+    </main>
+  );
 }
