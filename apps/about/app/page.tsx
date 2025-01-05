5755715bc670904d05bbddc1bf95979811b16b20
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-01-05 12:44:57 +0630

    setting about repo

---
+++ apps/about/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/components/ui/button";
+import {DemoComponent} from "~/components/demo";
+
+export default function Page() {
+  return (
+    <main>
+      <div>Hello World</div>
+    </main>
+  );
+}
