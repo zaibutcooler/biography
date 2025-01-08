5f88df24a97ee8cfcfe4a471e4c1c878580c5b0f
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-01-08 12:52:36 +0630

    added sitemap and robots file

--- apps/about/app/robots.ts
+++ apps/about/app/robots.ts
@@ -1,1 +1,18 @@
 // to setup robots
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "Googlebot",
+        allow: ["/"],
+      },
+      {
+        userAgent: ["Applebot", "Bingbot"],
+        disallow: ["/"],
+      },
+    ],
+    sitemap: `https://zaibutcooler.vercel.app/sitemap.xml`,
+  };
+}
--- apps/about/app/sitemap.ts
+++ apps/about/app/sitemap.ts
@@ -1,1 +1,13 @@
 // to setup sitemap
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://zaibutcooler.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+  ];
+}
