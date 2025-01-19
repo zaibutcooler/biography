3d27a85b11af015515d11ff7585f6d3a98cd65bd
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-01-19 13:31:38 +0630

    start contact form and gallery page

--- apps/about/app/contact/page.tsx
+++ apps/about/app/contact/page.tsx
@@ -1,4 +1,9 @@
 import {ContactForm} from "~/components/contact-form";
+import {Metadata} from "next";
+
+export const metadata:Metadata = {
+   title: "Contact"
+}
 
 export default function Page(){
   return <div>
--- apps/about/components/contact-form.tsx
+++ apps/about/components/contact-form.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import {z} from "zod";
+import {Form, FormField, FormItem} from "@repo/ui/components/ui/form";
+import {useForm} from "react-hook-form";
+import {Input} from "@repo/ui/components/ui/input";
+
+const schema = z.object({
+  email: z.string().email(),
+  content: z.string().min(10),
+})
+
+type FormType = z.infer<typeof schema>
 
 export const ContactForm: React.FC = () => {
-  return <div></div>
+  const form = useForm<FormType>({
+    resolver:zodResolver(schema),
+  })
+
+  const onSubmit = async (data:FormType) => {
+
+  }
+
+  return <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField name={"email"} control={form.control} render={
+          ({field}) => <FormItem>
+            <Input {...field} placeholder={"Email"}/>
+          </FormItem>
+        }/>
+
+        <FormField name={"content"} control={form.control} render={
+          ({field}) => <FormItem>
+            <Input {...field} placeholder={"Content"}/>
+          </FormItem>
+        }/>
+      </form>
+    </Form>
 }
