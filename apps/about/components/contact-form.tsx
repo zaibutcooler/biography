efb4fd626cb74815b3d3795c181c312c378d04c7
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-01-26 13:57:54 +0630

    updated metadata

--- apps/about/components/contact-form.tsx
+++ apps/about/components/contact-form.tsx
@@ -1,38 +1,56 @@
 import React from "react";
-import {z} from "zod";
-import {Form, FormField, FormItem} from "@repo/ui/components/ui/form";
-import {useForm} from "react-hook-form";
-import {Input} from "@repo/ui/components/ui/input";
+import { z } from "zod";
+import { Form, FormField, FormItem } from "@repo/ui/components/ui/form";
+import { useForm, } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@repo/ui/components/ui/input";
+import { Button } from "@repo/ui/components/ui/button";
 
 const schema = z.object({
   email: z.string().email(),
   content: z.string().min(10),
-})
+});
 
-type FormType = z.infer<typeof schema>
+type FormType = z.infer<typeof schema>;
 
 export const ContactForm: React.FC = () => {
   const form = useForm<FormType>({
-    resolver:zodResolver(schema),
-  })
+    resolver: zodResolver(schema),
+  });
 
-  const onSubmit = async (data:FormType) => {
+  const onSubmit = async (data: FormType) => {
+    try {
 
-  }
+    }catch (e){
+      console.log("ERROR ",e)
+    }
+  };
 
-  return <Form {...form}>
+  return (
+    <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField name={"email"} control={form.control} render={
-          ({field}) => <FormItem>
-            <Input {...field} placeholder={"Email"}/>
-          </FormItem>
-        }/>
+        <FormField
+          name={"email"}
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <Input {...field} placeholder={"Email"} />
+            </FormItem>
+          )}
+        />
 
-        <FormField name={"content"} control={form.control} render={
-          ({field}) => <FormItem>
-            <Input {...field} placeholder={"Content"}/>
-          </FormItem>
-        }/>
+        <FormField
+          name={"content"}
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <Input {...field} placeholder={"Content"} />
+            </FormItem>
+          )}
+        />
+
+        <Button type={"submit"}>Submit</Button>
       </form>
     </Form>
-}
+  );
+};
