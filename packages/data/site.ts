f5d556f3786d4a50a4e71ab90a7d1add390a017d
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-02-26 12:36:03 +0630

    setting containers and site urls

--- packages/data/site.ts
+++ packages/data/site.ts
@@ -1,3 +1,7 @@
 export const siteConfig = {
   productionUrl: "https://zaibutcooler.vercel.app",
+  resumeUrl: "https://www.linkedin.com/in/zaibotcooler",
+  aboutUrl: "https://www.linkedin.com/in/zaibotcooler",
+  contactUrl: "https://www.linkedin.com/in/zaibotcooler",
+  rootUrl: "https://zaibutcooler.vercel.app",
 };
---
+++ packages/ui/src/components/ui/container.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { cn } from "@repo/ui/lib/utils";
+import React from "react";
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const Container: React.FC<ContainerProps> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn("max-w-7xl mx-auto px-2 md:px-4 lg:px-6 ", className)}
+      {...props}
+    />
+  );
+};
