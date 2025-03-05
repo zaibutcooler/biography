f04b0f218be0a84c2c6321f9cb51c36e643f6719
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-03-05 23:34:57 +0630

    starting api repo

---
+++ packages/data/assets/images.ts
@@ -0,0 +1,6 @@
+const images = {
+  midnight:"https://5dtle1ixev.ufs.sh/f/NVoH0HnelgtLkFvMVCKjdDH1lbToV8KIBO0fnxsRw5MZ3QYq",
+}
+
+
+export default images;
--- packages/data/profile/index.ts
+++ packages/data/profile/index.ts
@@ -1,3 +1,5 @@
+import images from "../assets/images";
+
 const profile = {
   fullName: "Sai Ye Yint Aung",
   nickName: "Zai",
@@ -5,7 +7,7 @@ const profile = {
   locationLink: "https://www.google.com/maps/place/Taunggyi",
   about: "Turning ideas into products",
   summary: "I am a software engineer who loves to build scalable web and cloud solutions. I am also a co-founder of a startup that creates floorplans with diffusion models, co-founder of BMC (Burmese Machine Learning Community), and a lead contributor of a non-profit education startup (StudyCrew).",
-  avatar: "",
+  avatar: images.midnight,
   website: "https://zaibutcooler.vercel.app",
   email: "zaibutcooler@icloud.com",
   urls: {
